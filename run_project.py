b862a9c5b83693bc9cad564f730ee47f9977ac84
ShravaniDhavale <shravanidhavale1411@gmail.com>
2025-07-24 22:04:41 +0530

    Update run_project.py

--- run_project.py
+++ run_project.py
@@ -3,7 +3,7 @@ import webbrowser
 import os
 import time
 
-# Step 1: Start the Flask backend
+# Start the Flask backend
 print("Starting backend server...")
 
 backend_path = os.path.join(os.getcwd(), 'backend', 'app.py')
@@ -13,10 +13,10 @@ try:
 except Exception as e:
     print("Error starting backend:", e)
 
-# Step 2: Wait a moment to ensure the server starts
+# Wait a moment to ensure the server starts
 time.sleep(2)
 
-# Step 3: Open the frontend in the browser
+# Open the frontend in the browser
 frontend_path = os.path.join(os.getcwd(), 'frontend', 'Template', 'index.html')
 
 if os.path.exists(frontend_path):
